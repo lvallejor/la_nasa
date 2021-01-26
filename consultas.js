8e670fd7f2dff09b6ec2626d93825d45d95dd568
unknown <lvallejo.cl@gmail.com>
2021-01-26 14:47:36 -0300

    Vistas renderizadas

--- consultas.js
+++ consultas.js
@@ -29,4 +29,18 @@ const listaUsuarios = async () => {
   }
 };
 
-module.exports = { insertarUsuario, listaUsuarios };
+const checkAuth = async (id, auth) => {
+  const result = await pool.query(
+    "UPDATE usuarios SET auth=$1 where id = $2 RETURNING *",
+    [auth, id]
+  );
+  return result;
+};
+const getUser = async (email, password) => {
+  const result = await pool.query("SELECT * FROM usuarios where email = $1", [
+    email,
+  ]);
+  return result.rows[0];
+};
+
+module.exports = { insertarUsuario, listaUsuarios, checkAuth, getUser };
--- server.js
+++ server.js
@@ -2,8 +2,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
-const { insertarUsuario, listaUsuarios } = require("./consultas");
+const {
+  insertarUsuario,
+  listaUsuarios,
+  checkAuth,
+  getUser,
+} = require("./consultas");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 //Config
 app.listen(3001, () => console.log("Servidor en puerto 3001"));
@@ -18,6 +24,7 @@ app.engine(
     partialsDir: __dirname + "/views/components",
   })
 );
+const secretKey = "NASA";
 
 //Rutas
 app.get("/", (req, res) => {
@@ -32,4 +39,44 @@ app.post("/usuario", async (req, res) => {
 app.get("/Admin", async (req, res) => {
   const usuarios = await listaUsuarios();
   res.render("Admin", { layout: "Admin", usuarios });
+});
+
+app.post("/auth", async (req, res) => {
+  const { id, auth } = req.body;
+  try {
+    const result = await checkAuth(id, auth);
+    res.send(result);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("Login", { layout: "Login" });
+});
+
+app.post("/verify", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await getUser(email, password);
+  let token = user && user.auth ? jwt.sign(user, secretKey) : false;
+  user
+    ? user.auth
+      ? res.redirect("/Evidencias?token=" + token)
+      : res.send("Usted no está autorizado")
+    : res.send("No existe este usuario en nuestra base de datos");
+
+  res.send();
+});
+
+app.get("/Evidencias", (req, res) => {
+  const { token } = req.query;
+
+  jwt.verify(token, secretKey, (err, payload) => {
+    err
+      ? res.status(401).send({ error: "401 No Autorizado", message: err })
+      : res.render("Evidencias", {
+          layout: "Evidencias",
+          nombre: payload.nombre,
+        });
+  });
 });
